3305c690708b2c6201e40f17c8c37a7fe4f0eac0
mbouridev <mbouri.dev@gmail.com>
2021-12-16 11:30:11 +0100

    ajout participant

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,33 @@
-console.log("hello world");
+
+
+function addParticipant(event)
+{
+    event.preventDefault();
+
+    const nameInputElt = document.getElementById("nameInput");
+    const participantName = nameInputElt.value.trim();
+
+    if (participantName === '') {
+        alert('Veuillez saisir un prénom');
+        return;
+    }
+
+    const participantElt = `
+    <li>${participantName}</li>
+    `;
+
+    nameInputElt.value = '';
+
+    const participantListElt = document.getElementById('participantList');
+
+    participantListElt.innerHTML = participantListElt.innerHTML + participantElt;
+}
+
+
+
+const addNameFormElt = document.getElementById("addNameForm");
+addNameFormElt.addEventListener("submit", addParticipant);
+
+
+
+
